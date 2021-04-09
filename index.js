b3ed3fece57913713e420a79dc65f0362ec91a23
asco-test <77448500+asco-test@users.noreply.github.com>
2021-04-09 08:31:03 +0200

    Add initial dietician website files

---
+++ index.js
@@ -0,0 +1,11 @@
+var navbar = document.querySelector('header')
+
+window.onscroll = function() {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+}
